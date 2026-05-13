0264f43d357617d5c228cd7e399b3be241b13919
Duran <jose.duran@uamv.edu.ni>
2026-05-13 11:53:42 -0600

    Sesión 13 mayo

---
+++ Matriz1/Program.cs
@@ -0,0 +1,30 @@
+﻿//Matriz 3x3
+int[,] matriz = new int[3, 3];
+
+Console.WriteLine("Digite os elementos da matriz 3x3:");
+for (int f = 0; f < 3; f++)
+{
+    for (int c = 0; c < 3; c++)
+    {
+        Console.Write($"Elemento [{f},{c}]: ");
+        matriz[f, c] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("Mostrar Matriz");
+for (int f = 0; f < 3; f++)
+{
+    for (int c = 0; c < 3; c++)
+    {
+        if (matriz[f, c] % 2 == 0){
+            Console.ForegroundColor = ConsoleColor.Green;
+        } else {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        if(matriz[f, c] < 0) Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write($"{matriz[f, c]} ");
+        Console.ResetColor();
+    }
+    Console.WriteLine();
+}
+
